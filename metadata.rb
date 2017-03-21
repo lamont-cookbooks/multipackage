61f4891a33bb39db034d60781b6570fc46d5c3a9
Lamont Granquist <lamont@scriptkiddie.org>
2017-03-21 08:08:03 -0700

    version bump

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ source_url "https://github.com/lamont-cookbooks/multipackage"
 issues_url "https://github.com/lamont-cookbooks/multipackage/issues"
 description "Accumulated installation of multiple packages across multiple cookbooks"
 long_description "Accumulated installation of multiple packages across multiple cookbooks"
-version "4.0.0"
+version "4.0.1"
 name "multipackage"
 
 depends "compat_resource"
