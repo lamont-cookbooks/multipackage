5a1a0e826895a4cebbd774c1773a6fe138e2e990
Tim Smith <tsmith@chef.io>
2018-06-05 13:04:24 -0700

    Resolve foodcritic warnings
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,13 @@
 maintainer "Lamont Granquist"
 maintainer_email "lamont@scriptkiddie.org"
-license "Apache 2.0"
+license "Apache-2.0"
 source_url "https://github.com/lamont-cookbooks/multipackage"
 issues_url "https://github.com/lamont-cookbooks/multipackage/issues"
 description "Accumulated installation of multiple packages across multiple cookbooks"
 long_description "Accumulated installation of multiple packages across multiple cookbooks"
 version "5.0.0"
 name "multipackage"
+
+supports "any"
 
 chef_version ">= 13.0.0"
